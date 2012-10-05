4ed3fcce60d7ee719ec17be6278576f0a056120e
Kirill Simonov <xi@resolvent.net>
2012-10-05 12:39:38 -0500

    Fixed the setup script to include the package source.

--- setup.py
+++ setup.py
@@ -8,12 +8,23 @@ from setuptools import setup, find_packages
 
 
 NAME = "Cogs"
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 DESCRIPTION = """A toolkit for developing command-line utilities in Python"""
 LONG_DESCRIPTION = open('README', 'r').read()
 AUTHOR = """Kirill Simonov (Prometheus Research, LLC)"""
+AUTHOR_EMAIL = "xi@resolvent.net"
 LICENSE = "MIT"
-PACKAGES = find_packages()
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python :: 2 :: Only",
+    "Topic :: Utilities",
+]
+PACKAGES = find_packages('src')
 PACKAGE_DIR = {'': 'src'}
 INSTALL_REQUIRES = ['setuptools', 'PyYAML']
 ENTRY_POINTS = {
@@ -29,6 +40,7 @@ setup(name=NAME,
       description=DESCRIPTION,
       author=AUTHOR,
       license=LICENSE,
+      classifiers=CLASSIFIERS,
       packages=PACKAGES,
       package_dir=PACKAGE_DIR,
       install_requires=INSTALL_REQUIRES,
