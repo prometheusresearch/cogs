4efa799071222d0657579bec4aa3f187992cd339
Kirill Simonov <xi@resolvent.net>
2019-03-20 10:29:53 -0500

    bumped the version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 
 NAME = "Cogs"
-VERSION = "0.4.3"
+VERSION = "0.4.4"
 DESCRIPTION = """Toolkit for developing command-line utilities in Python"""
 LONG_DESCRIPTION = open('README', 'r').read()
 AUTHOR = """Kirill Simonov (Prometheus Research, LLC)"""
